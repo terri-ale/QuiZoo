7df3bd8a07f225474031bd6d7dd08f1f6ea31365
terri-ale <alexterribas@gmail.com>
2020-12-22 14:12:16 +0100

    Estructura de paquetes creada, con modelo de vista - controlador.

--- app/src/main/java/com/example/quizoo/MainActivity.java
+++ app/src/main/java/com/example/quizoo/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.quizoo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import com.example.quizoo.receiver.BatteryReceiver;
+
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter batteryIntentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        batteryIntentFilter=new IntentFilter();
+        batteryIntentFilter.addAction(Intent.ACTION_BATTERY_LOW);
     }
+
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        registerReceiver(new BatteryReceiver(), batteryIntentFilter);
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        unregisterReceiver(broadcast);
+    }
+
 }
