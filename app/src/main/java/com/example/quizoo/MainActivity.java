d4673796d516a8459740052438d57e8e5a3ac1ab
rhgarcia88 <rhgarguer@gmail.com>
2020-12-22 11:39:55 +0100

    Icono actualizado, Splash Screen establecida y NavGraph y Navhost creados.

--- app/src/main/java/com/example/quizoo/MainActivity.java
+++ app/src/main/java/com/example/quizoo/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.quizoo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,HostActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, 1500);
+
     }
 }
