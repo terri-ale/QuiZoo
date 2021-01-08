8b84216954be1ce8340ab4da14e59c346c57ce27
Miguel Angel <51151697+MiguelAngelLopezBlanca@users.noreply.github.com>
2021-01-08 09:55:10 +0100

    Base de datos interna, Room y entity

--- app/src/main/java/com/example/quizoo/model/dao/UserDao.java
+++ app/src/main/java/com/example/quizoo/model/dao/UserDao.java
@@ -1,4 +1,24 @@
 package com.example.quizoo.model.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.quizoo.model.entity.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    @Update
+    int update(User user);
+
+    @Insert
+    long insert(User user);
+
+    @Query("select * from users")
+    LiveData<List<User>> getAll();
+
+
 }
--- app/src/main/java/com/example/quizoo/model/entity/User.java
+++ app/src/main/java/com/example/quizoo/model/entity/User.java
@@ -1,4 +1,91 @@
 package com.example.quizoo.model.entity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
 public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+
+    @NonNull
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @Nullable
+    @ColumnInfo(name = "avatar")
+    private int avatar;
+
+    @Nullable
+    @ColumnInfo(name = "numResponses")
+    private int numResponses;
+
+    @Nullable
+    @ColumnInfo(name = "numResponsesCorrect")
+    private int numResponsesCorrect;
+
+    public User(@NonNull String name, int avatar, int numResponses, int numResponsesCorrect) {
+        this.name = name;
+        this.avatar = avatar;
+        this.numResponses = numResponses;
+        this.numResponsesCorrect = numResponsesCorrect;
+    }
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    public int getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(int avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getNumResponses() {
+        return numResponses;
+    }
+
+    public void setNumResponses(int numResponses) {
+        this.numResponses = numResponses;
+    }
+
+    public int getNumResponsesCorrect() {
+        return numResponsesCorrect;
+    }
+
+    public void setNumResponsesCorrect(int numResponsesCorrect) {
+        this.numResponsesCorrect = numResponsesCorrect;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", avatar=" + avatar +
+                ", numResponses=" + numResponses +
+                ", numResponsesCorrect=" + numResponsesCorrect +
+                '}';
+    }
 }
