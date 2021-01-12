0f24544a37bec375c745acf03b57a4bcc3832bd7
DVirgosCanon <dvirgos318@ieszaidinvergeles.org>
2021-01-12 12:01:34 +0100

    Los metodos para las preferencias compartidas, uno para comprobar si existen y otro para crearlas. Lo unico que tenemos que hacer es pasarle la contraseña y las sharedprefenrences que he indicado como se hacen.

--- app/src/main/java/com/example/quizoo/model/Repository.java
+++ app/src/main/java/com/example/quizoo/model/Repository.java
@@ -1,4 +1,25 @@
 package com.example.quizoo.model;
 
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class Repository {
+
+
+    public boolean CompruebaSharedPreferences (SharedPreferences preferences) {
+        String password = preferences.getString("password", "N/D");
+        if (password.equals("N/D")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void crearSharedPreferences (SharedPreferences preferences, String password) {
+        preferences.edit().putString("password", password).apply();
+
+        //SharedPreferences preferences = getSharedPreferences("datosAdmin", Context.MODE_PRIVATE);
+    }
+
 }
