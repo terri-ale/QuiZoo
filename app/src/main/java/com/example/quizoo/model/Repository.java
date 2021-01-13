bff607166ab044e75bee78ace9bbc0a81f22debf
DVirgosCanon <dvirgos318@ieszaidinvergeles.org>
2021-01-13 09:02:42 +0100

    lo de ayer con pequeños cambios

--- app/src/main/java/com/example/quizoo/model/Repository.java
+++ app/src/main/java/com/example/quizoo/model/Repository.java
@@ -2,24 +2,27 @@ package com.example.quizoo.model;
 
 
 import android.content.Context;
+
 import android.content.SharedPreferences;
+
 
 public class Repository {
 
+    private Context context;
 
-    public boolean CompruebaSharedPreferences (SharedPreferences preferences) {
-        String password = preferences.getString("password", "N/D");
-        if (password.equals("N/D")) {
-            return true;
-        } else {
+    public boolean checkSharedPreferences () {
+        SharedPreferences preferences = context.getSharedPreferences("datosAdmin", Context.MODE_PRIVATE);;
+        String password = preferences.getString("password", "");
+        if (password.equals("")) {
             return false;
+        } else {
+            return true;
         }
     }
 
-    public void crearSharedPreferences (SharedPreferences preferences, String password) {
+    public void createSharedPreferences (String password) {
+        SharedPreferences preferences = context.getSharedPreferences("datosAdmin", Context.MODE_PRIVATE);;
         preferences.edit().putString("password", password).apply();
-
-        //SharedPreferences preferences = getSharedPreferences("datosAdmin", Context.MODE_PRIVATE);
     }
 
 }
