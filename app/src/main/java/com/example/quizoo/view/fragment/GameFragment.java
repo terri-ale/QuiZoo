b52a2626f6306620395b5cf3074c3d157cc33aab
Miguel Angel <51151697+MiguelAngelLopezBlanca@users.noreply.github.com>
2021-01-19 16:18:24 +0100

    Dialogo con las Instrucciones del juego en GameFragment(Texto que se podría modificar)

--- app/src/main/java/com/example/quizoo/view/fragment/GameFragment.java
+++ app/src/main/java/com/example/quizoo/view/fragment/GameFragment.java
@@ -1,15 +1,28 @@
 package com.example.quizoo.view.fragment;
 
+import android.app.Dialog;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.AlertDialogLayout;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.text.Html;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.quizoo.R;
 import com.example.quizoo.viewmodel.ViewModelActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import static android.text.Html.FROM_HTML_MODE_LEGACY;
 
 
 public class GameFragment extends Fragment {
@@ -34,4 +47,33 @@ public class GameFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FloatingActionButton btHlep = view.findViewById(R.id.btHelp);
+        btHlep.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                instruccionesDialog(savedInstanceState);
+            }
+        });
+
+    }
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        public Dialog instruccionesDialog(Bundle savedInstanceState) {
+
+            String text = getString(R.string.instrucciones);
+            Spanned styledText = Html.fromHtml(text, FROM_HTML_MODE_LEGACY);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(styledText);
+            builder.show();
+            return builder.create();
+        }
+
+
 }
