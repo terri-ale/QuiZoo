9385e9a0f9ccf9315907cb6cfb49212c98e6ca96
rhgarcia88 <rhgarguer@gmail.com>
2021-01-27 12:44:02 +0100

    Animacion Carta Basica y Nav Bottom

--- app/src/main/java/com/example/quizoo/view/fragment/GameFragment.java
+++ app/src/main/java/com/example/quizoo/view/fragment/GameFragment.java
@@ -29,6 +29,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.quizoo.R;
 import com.example.quizoo.rest.pojo.Card;
