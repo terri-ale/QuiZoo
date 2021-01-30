a040b34d365c736a251e1c28befeaffa2be74eea
rhgarcia88 <rhgarguer@gmail.com>
2021-01-30 11:07:29 +0100

    Ajustes

---
+++ app/src/main/java/com/example/quizoo/view/adapter/QuestionsAdapter.java
@@ -0,0 +1,69 @@
+package com.example.quizoo.view.adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quizoo.R;
+import com.example.quizoo.rest.pojo.Card;
+import com.example.quizoo.rest.pojo.Question;
+import com.example.quizoo.viewmodel.ViewModelActivity;
+
+import java.util.List;
+
+public class QuestionsAdapter extends RecyclerView.Adapter<QuestionsAdapter.ViewHolder> {
+
+    private List<Question> questionList;
+    private Activity activity;
+
+    public QuestionsAdapter(List<Question> questionList, Activity activity) {
+        this.questionList = questionList;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_question_recycler, parent, false);
+       ViewHolder holder = new ViewHolder(vista);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+                        holder.tvPregunta.setText(questionList.get(position).getText());
+                        holder.constraintLayout.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return questionList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvPregunta;
+        ConstraintLayout constraintLayout;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvPregunta = itemView.findViewById(R.id.tvPreguntaR);
+            constraintLayout = itemView.findViewById(R.id.questionsLayout);
+
+        }
+    }
+}
